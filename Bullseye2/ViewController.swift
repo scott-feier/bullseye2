a95f728458eb52be73db09822b09fd5e958c6384
Scott Feier <scott.feier@fakemail.com>
2020-02-19 13:21:13 -0500

    initial commit for Part 1 - one button so far

--- Bullseye2/ViewController.swift
+++ Bullseye2/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func showAlert() {
+        print("Hello!")
+    }
 
 }
 
